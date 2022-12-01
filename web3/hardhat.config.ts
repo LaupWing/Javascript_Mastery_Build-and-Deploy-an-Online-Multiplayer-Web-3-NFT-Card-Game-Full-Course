957c864307ef1da255debcebca053177ec8ca4b8
LaupWing <loc_strict@hotmail.com>
2022-12-01 13:05:03 +0100

    config file

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,8 +1,28 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+   solidity: {
+      version: "0.8.17",
+      settings:{
+         viaIR: true,
+         optimizer:{
+            enabled: true,
+            runs: 100
+         }
+      }
+   },
+   networks:{
+      fuji:{
+         url: "https://api.avax-test.network/ext/bc/C/rpc",
+         gasPrice: 225000000000,
+         chainId: 431113,
+         accounts: [process.env.PRIVATE_KEY!]
+      }
+   }
+}
 
-export default config;
+export default config
