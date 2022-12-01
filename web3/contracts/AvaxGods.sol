a8359592ffa645ffa3867866450a863ebc0e021d
LaupWing <loc_strict@hotmail.com>
2022-12-01 13:27:30 +0100

    added structs

---
+++ web3/contracts/AvaxGods.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
+   string public baseURI;
+   uint256 public totalSupply;
+   uint256 public constant DEVIL = 0;
+   uint256 public constant FIREBIRD = 2;
+   uint256 public constant KAMO = 3;
+   uint256 public constant KUKULKAN = 4;
+   uint256 public constant CELESTION = 5;
+
+   uint256 public constant MAX_ATTACK_DEFEND_STRENGTH = 10;
+
+   enum BattleStatus { 
+      PENDING, 
+      STARTED, 
+      ENDED 
+   }
+
+   struct GameToken {
+      string name;
+      uint256 id;
+      uint256 atttackStrength;
+      uint256 defenseStrength;
+   }
+
+   struct Player {
+      address playerAddress;
+      string playerName;
+      uint256 playerMana;
+      uint256 playerHealth;
+      bool inBattle;
+   }
+
+   struct Battle {
+      BattleStatus battleStatus;
+      bytes32 battleHash;
+      string name;
+      address[2] players;
+      uint8[2] moves;
+      address winner;
+   }
+}
+
